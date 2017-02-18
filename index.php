d8aa105bca2fc67cf332c1a20cca212990163577
indra <indra.rahmat88@gmail.com>
2017-02-18 20:35:30 +1100

    update motto and footer

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
           <div class="carousel-caption irkom-caption">
             <h1>Personal Website and Blog</h1>
             <hr>
-            <p>We have something to share and remember everything</p>
+            <p>Simple personal webiste to share and remember experienced</p>
           </div>
         </div>
       </div>
@@ -21,9 +21,9 @@
         <img class="second-slide img-responsive" src="<?php bloginfo('template_url'); ?>/img/second-slide.jpg" alt="Second slide">
         <div class="container">
           <div class="carousel-caption irkom-caption">
-            <h1>Learning by Doing </h1>
+            <h1>Learning by Practicing </h1>
             <hr>
-            <p>Simple blog and website to share knowledge and information</p>
+            <p>Simple blog to improve your skills. easy to practiced and source code provided</p>
             <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
           </div>
         </div>
@@ -61,45 +61,5 @@
     </div>
   </div>
   <!-- end section b-->
-
-  <div class="page-header text-center">
-    <h2>Blogs</h2>
-  </div>
-
-  <div class="section-blog">
-    <div class="container">
-      <?php if(have_posts()) : ?>
-      <div class="row">
-        <?php while(have_posts()) : the_post(); ?>
-          <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="panel panel-primary">
-                  <!-- Default panel contents -->
-              <div class="panel-heading"><?php the_title(); ?></div>
-              <?php if(has_post_thumbnail()) : ?>
-                <div class="irkom-post-thumbnails">
-                  <?php the_post_thumbnail(); ?>
-                </div>
-                <?php else : ?>
-                  <img data-src="holder.js/350x263/auto/#771:#100" alt="First slide">
-              <?php  endif; ?>
-
-              <div class="panel-body">
-                <p><?php the_excerpt(); ?></p>
-                <a class="btn btn-info" href="<?php the_permalink(); ?>" role="button">Read more »</a>
-              </div>
-              <div class="panel-footer text-left">
-               <img style="background-image:url(http://placekitten.com/g/400/200)" class="card-author-circle">
-                  <?php the_author(); ?> | <?php the_time('F j, Y g:i a'); ?>
-             </div>
-            </div>
-          </div>
-
-        <?php endwhile; ?>
-      </div>
-      <?php else: ?>
-      <p><?php __('No Posts found'); ?></p>
-    <?php endif; ?>
-    </div>
-  </div>
 
 <?php get_footer(); ?>
