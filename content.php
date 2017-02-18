3930b6cf2b1f159ac6e972167427726f2b37324b
indra <indra.rahmat88@gmail.com>
2017-02-18 23:56:18 +1100

    added link to details

--- content.php
+++ content.php
@@ -10,7 +10,7 @@
         <img data-src="holder.js/800x360/auto/#771:#100" alt="First slide">
     <?php endif; ?>
     <div class="panel-body text-left">
-        <a class="" href="#" role="">lengkapnya »</a>
+        <a class="" href="<?php the_permalink(); ?>" role="">lengkapnya »</a>
     </div>
     <div class="panel-footer text-left ">
         <img style="background-image:url(http://placekitten.com/g/400/200)" class="stories-author-circle"> Writer
